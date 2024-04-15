d28338b966a977fe85f08bf85841cf67484d531c
matzkinfeli <141249991+matzkinfeli@users.noreply.github.com>
2024-04-14 21:21:51 -0300

    Create node.java

---
+++ TP2/node.java
@@ -0,0 +1,9 @@
+package anillo;
+
+public abstract class Node {
+
+    public abstract Object getData();
+    public abstract Node getNext();
+    public abstract void removeNext();
+    public abstract Node addNext(Object data);
+}
