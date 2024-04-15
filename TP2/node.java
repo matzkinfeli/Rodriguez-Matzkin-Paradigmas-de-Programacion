5d2cd37a1f3fdfceccac327c1c8d33b40ed319e0
matzkinfeli <141249991+matzkinfeli@users.noreply.github.com>
2024-04-14 21:22:54 -0300

    Delete TP2/node.java

--- TP2/node.java
+++
@@ -1,9 +0,0 @@
-package anillo;
-
-public abstract class Node {
-
-    public abstract Object getData();
-    public abstract Node getNext();
-    public abstract void removeNext();
-    public abstract Node addNext(Object data);
-}
